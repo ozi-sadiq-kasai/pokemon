3f48b361a21937a00c9de077d0c9026ea68a6b47
ozi <ozisadiqkasai@gmail.com>
2023-03-03 16:04:47 +0100

    toggle pikachu image

--- pokemon/app.js
+++ pokemon/app.js
@@ -7,11 +7,13 @@
 
 // pikachu
 document.querySelector('#pikachu').addEventListener('click',run)
+let pikImage = document.querySelector('#pik')
 function run(){
   let pika = document.querySelector('#pikachu').innerText
   let msg = new SpeechSynthesisUtterance();
   msg.text = pika
   window.speechSynthesis.speak(msg);
+  pikImage.classList.toggle("hidden")
 }
 
 
