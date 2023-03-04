5e0264cd8858bc7312a3493ef6617ff088fa87c8
ozi <ozisadiqkasai@gmail.com>
2023-03-04 21:33:26 +0100

    add background image

--- pokemon/app.js
+++ pokemon/app.js
@@ -1,20 +1,43 @@
 // things to do
 // scale pokemon
-// mouse movement on image
 // computer say name
 // on click make image background change color
 
 
 // pikachu
 document.querySelector('#pikachu').addEventListener('click',run)
-let pikImage = document.querySelector('#pik')
+// let pikImage = document.querySelector('#pik')
+
 function run(){
   let pika = document.querySelector('#pikachu').innerText
   let msg = new SpeechSynthesisUtterance();
   msg.text = pika
   window.speechSynthesis.speak(msg);
-  pikImage.classList.toggle("hidden")
+  // pikImage.classList.toggle("hidden")
+ 
+
+  // background display
+  document.querySelector('body').style.background = "gray"
+  ;
+  document.querySelector('body').style.backgroundImage= 'url("https://www.animatedimages.org/data/media/1446/animated-pokemon-image-0016.gif")'; 
+  
 }
 
+// charmander
+// document.querySelector('#charmander').addEventListener('click',run)
+// // let charImage = document.querySelector('#char')
+// function run(){
+//   let char = document.querySelector('#charmander').innerText
+//   let msg = new SpeechSynthesisUtterance();
+//   msg.text = char
+//   window.speechSynthesis.speak(msg);
+//   // charImage.classList.toggle("hidden")
 
-;
+//   // background display
+//   document.querySelector('body').style.background = "gray";
+//   document.querySelector('body').style.backgroundImage= 'url("https://www.animatedimages.org/data/media/1446/animated-pokemon-image-0007.gif")'; 
+  
+// }
+
+
+
